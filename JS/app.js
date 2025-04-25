b6802444bd31c2e5b93a72d1517ee036982b14ba
Birka <birkavidit@gmail.com>
2025-04-25 11:54:53 +0200

    added enable disbale tags btns, added animation to removing articles

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,17 @@
+class Tag
+{
+    constructor(html, active, articles = [], btnHtml = undefined)
+    {
+        this.html = html;
+        this.active = active;
+        this.articles = articles;
+        this.btnHtml = btnHtml;
+    }
+}
+
 const list = document.querySelector("main nav div ul");
 const tags = new Map();
+const tagContainer = document.querySelector("main>section>ul");
 const articles = document.querySelectorAll("main>article");
 
 articles.forEach(article => {
@@ -29,65 +41,125 @@ articles.forEach(article => {
             td.articles.push(articleData);
             return;
         }
-        const tagData = {
-            html: tag,
-            active: true,
-            articles: []
-        };
+        const tagData = new Tag(tag, true);
         tags.set(tag.innerText, tagData);
 
         articleData.tags.push(tagData);
         tagData.articles.push(articleData);
+
+        initTagButton(tagData);
     })
     // console.log(articleData.html.querySelector("h1").innerText, articleData.tags.map(t=>{return {name: t.html.innerText, arts: t.articles.map(a=> a.html.querySelector("h1").innerText)}}));
 
 });
- const tagContainer = document.querySelector("main>section>ul");
-// const enablers = tagContainer.parentElement().querySelectorAll("div p");
-// enablers[0].
-tags.forEach(tag => {
 
-    const htmlTag = document.createElement("li");
-    htmlTag.classList.add("clickable");
-    htmlTag.classList.add("btn");
-    htmlTag.innerText = tag.html.innerText;
-    tagContainer.appendChild(htmlTag);
-    htmlTag.classList.add("activeTag");
-    htmlTag.style.setProperty(
+//INIT Enablers
+const enablers = tagContainer.parentNode.querySelectorAll("div p");
+enablers[0].addEventListener('click', ()=>{
+    tags.forEach(t => {
+        enableTag(t)
+
+    });
+});
+
+enablers[1].addEventListener('click', ()=>{
+    tags.forEach(t => {
+        disableTag(t)
+
+    });
+});
+
+function initTagButton(tag)
+{
+    const htmlTagBtn = document.createElement("li");
+    htmlTagBtn.classList.add("clickable");
+    htmlTagBtn.classList.add("btn");
+    htmlTagBtn.innerText = tag.html.innerText;
+    tagContainer.appendChild(htmlTagBtn);
+    htmlTagBtn.classList.add("activeTag");
+    htmlTagBtn.style.setProperty(
         'padding-top', '2px'
     )
-    htmlTag.style.setProperty(
+    htmlTagBtn.style.setProperty(
         'padding-bottom', '2px'
     )
-    htmlTag.addEventListener("click", () => {
-        const current = tags.get(htmlTag.innerText);
-        current.active = !current.active;
-
-        if (current.active) {
-            current.articles.forEach(article => {
-                article.html.classList.remove("hidden");
-                article.navHtml.classList.remove("hidden");
-            });
-            htmlTag.classList.add("activeTag");
-
-            return;
-        }
-        htmlTag.classList.remove("activeTag");
-
-        //if any article of this tag has no active tags
-        const closedArticles = current.articles.filter(a => !a.tags.some(t => t.active))
-        if (closedArticles.length != 0) {
-            closedArticles.forEach(a => {
-                a.html.classList.add("hidden");
-                a.navHtml.classList.add("hidden");
-
-            });
-        }
+    tag.btnHtml = htmlTagBtn;
+    htmlTagBtn.addEventListener("click", () => {
+        const current = tags.get(htmlTagBtn.innerText);
+        if(current.active) disableTag(current);
+        else enableTag(current);
     })
-});
+
+}
+// document.addEventListener('keypress', function (e) {
+//     if (e.key === 'l') {
+//         hideElement(articles[0]);
+//     }
+// });
+function enableTag(tag)
+{
+    if(tag.active) return;
+    tag.active = true;
+    tag.articles.forEach(article => {
+        showElement(article.html);
+        showElement(article.navHtml);
+    });
+    tag.btnHtml.classList.add("activeTag");
+
+}
+
+function disableTag(tag)
+{
+    if(!tag.active) return;
+
+    tag.active = false;
+    tag.btnHtml.classList.remove("activeTag");
+
+    //if any article of this tag has no active tags
+    const closedArticles = tag.articles.filter(a => !a.tags.some(t => t.active))
+    if (closedArticles.length == 0) return;
+
+    closedArticles.forEach(a => {
+        console.log(a.html);
+        
+        hideElement(a.html);
+        hideElement(a.navHtml);
+    });
+}
+
+function hideElement(el) {
+    const sectionHeight = el.scrollHeight;
+
+    el.style.height = sectionHeight + "px"; 
+    el.offsetHeight; 
+    el.classList.add("hide-animate");
+    el.style.height = "0px";
+
+    el.addEventListener("transitionend", function handler() {
+        el.style.display = "none";
+        el.classList.remove("hide-animate");
+        el.style.height = "";
+        el.removeEventListener("transitionend", handler);
+    });
+}
+
+function showElement(el) {
+    if(el.style.display !== "none") return;
+    el.style.display = "block"; 
+    const sectionHeight = el.scrollHeight;
+    el.style.height = "0px"; 
+    el.offsetHeight; 
+
+    el.style.height = sectionHeight + "px";
+    el.classList.add("show-animate");
+
+    el.addEventListener("transitionend", function handler() {
+        el.style.height = "auto";
+        el.classList.remove("show-animate");
+        el.removeEventListener("transitionend", handler);
+    });
+}
 
 var upButton = document.querySelector("main button img").addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
-    console.log("up");
-
 });
