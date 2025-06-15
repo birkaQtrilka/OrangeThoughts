c30d2b11da316b1c432a5869b08f1403053fed0e
Birka <birkavidit@gmail.com>
2025-06-15 17:33:41 +0200

    background re-design, layout fixes

--- JS/header.ts
+++ JS/header.ts
@@ -3,13 +3,13 @@ window.addEventListener("DOMContentLoaded", () => {
     const header = document.createElement("header");
     header.innerHTML = `
         <a href="index.html#top">
-            <img src="Images/Logo.png" alt="pfp">
+            <img src="/Images/Logo.png" alt="pfp">
         </a>
         <nav>
             <ul id="pages">
-                <li><a class="orangeAnim" href="index.html#AboutMe">About</a></li>
-                <li><a class="orangeAnim" href="index.html#Projects">Projects</a></li>
-                <li><a class="orangeAnim" href="Coding adventures.html">Adventures</a></li>
+                <li><a class="orangeAnim" href="/index.html#AboutMe">About</a></li>
+                <li><a class="orangeAnim" href="/index.html#Projects">Projects</a></li>
+                <li><a class="orangeAnim" href="/Coding adventures.html">Adventures</a></li>
             </ul>
             <ul>
                 <li>
@@ -99,7 +99,7 @@ window.addEventListener("DOMContentLoaded", () => {
         </nav>
     `;
     
-    getBodyWrapper().prepend(header);
+    document.body.prepend(header);
     const emailBtn = document.getElementById("emailBtn");
     
     if (emailBtn) {
@@ -130,7 +130,7 @@ window.addEventListener("DOMContentLoaded", () => {
     }
 
     const footer = document.createElement("footer");
-
+    
     footer.innerHTML = `
     <footer>
         <ul>
@@ -139,25 +139,25 @@ window.addEventListener("DOMContentLoaded", () => {
         </ul>
         <ul>
             <li><a href="https://www.instagram.com/orange_thoughtss/" target="_blank">
-                <img class="btn orangeImgAnim" src="Images/instaIcon.png" alt="Instagram">
+                <img class="btn orangeImgAnim" src="/Images/instaIcon.png" alt="Instagram">
             </a></li>
             <li><a href="https://www.linkedin.com/in/stefan-carpeliuc-b2880427b/" target="_blank">
-                <img class="btn orangeImgAnim" src="Images/LinkedInIcon.png" alt="LinkedIn">
+                <img class="btn orangeImgAnim" src="/Images/LinkedInIcon.png" alt="LinkedIn">
             </a></li>
             <li><a href="https://x.com/Birkator" target="_blank">
-                <img class="btn orangeImgAnim" src="Images/TwitterIcon.png" alt="Twitter">
+                <img class="btn orangeImgAnim" src="/Images/TwitterIcon.png" alt="Twitter">
             </a></li>
             <li><a href="https://github.com/birkaQtrilka" target="_blank">
-                <img class="btn orangeImgAnim" src="Images/gitHubIcon.png" alt="GitHub">
+                <img class="btn orangeImgAnim" src="/Images/gitHubIcon.png" alt="GitHub">
             </a></li>
             <li><a href="https://www.youtube.com/channel/UCuIZf3WkVrkOhyHA0Tin4gg" target="_blank">
-                <img class="btn orangeImgAnim" src="Images/icons8-youtube-100.png" alt="YouTube">
+                <img class="btn orangeImgAnim" src="/Images/icons8-youtube-100.png" alt="YouTube">
             </a></li>
         </ul>
     </footer>
     `;
 
-    getBodyWrapper().append(footer);
+    document.body.append(footer);
     
 });
 
