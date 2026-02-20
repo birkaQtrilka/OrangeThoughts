58ef6a2a96524a122f7f17756ad6ff8f8a5b07f3
Stefan Carpeliuc <birkavidit@gmail.com>
2026-02-20 21:48:37 +0100

    #19 fixed instagram link, made an array of socials for footer

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,5 +1,12 @@
 import {Component, Input} from '@angular/core';
 
+interface FooterLink {
+  href: string;
+  icon?: string;
+  alt?: string;
+}
+
+
 @Component({
   selector: 'app-footer',
   imports: [],
@@ -8,4 +15,32 @@ import {Component, Input} from '@angular/core';
 })
 export class Footer {
   @Input() email: string = 'carpeliucstefan@gmail.com';
+
+  socialLinks: FooterLink[] = [
+    {
+      href: 'https://www.instagram.com/orange.thoughts.dev/',
+      icon: 'assets/Images/Icons/instaIcon.png',
+      alt: 'Instagram'
+    },
+    {
+      href: 'https://www.linkedin.com/in/stefan-carpeliuc-b2880427b/',
+      icon: 'assets/Images/Icons/LinkedInIcon.png',
+      alt: 'LinkedIn'
+    },
+    {
+      href: 'https://x.com/Birkator',
+      icon: 'assets/Images/Icons/TwitterIcon.png',
+      alt: 'Twitter'
+    },
+    {
+      href: 'https://github.com/birkaQtrilka',
+      icon: 'assets/Images/Icons/gitHubIcon.png',
+      alt: 'GitHub'
+    },
+    {
+      href: 'https://www.youtube.com/channel/UCuIZf3WkVrkOhyHA0Tin4gg',
+      icon: 'assets/Images/Icons/icons8-youtube-100.png',
+      alt: 'YouTube'
+    }
+  ];
 }
